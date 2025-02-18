139be461bd8443acbf0f4ffbc6aa8b8275c3be6b
victorfaruna <farunavictor043@gmail.com>
2025-02-17 17:56:35 -0800

    update

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,13 @@
-import Image from "next/image";
+// import Image from "next/image";
 import React from "react";
 
 export default function Logo() {
   return (
     <div className="flex items-center">
       {/*  */}
-      <div className="font-bold leading-none flex text-[22px] items-end gap-[1px] text-color-3">
-        <Image
-          className="size-[24px] object-contain"
-          src={"/images/logo.svg"}
-          width={20}
-          height={20}
-          alt="logo"
-        />
-        <p className="text-black">imme.</p>
+      <div className="font-bold leading-none flex text-[22px] items-end text-color-3">
+        <span className="text-color-3">G</span>
+        <span className="text-black">imme.</span>
       </div>
     </div>
   );
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import Image from "next/image";
+import React, { useMemo } from "react";
 
 export default function Testimonials() {
+  const TESTIMONIALS = useMemo(() => {
+    return [
+      {
+        name: "John Doe",
+        image: "/images/user-1.webp",
+        text: "Lorem ipsum dolor sit! Sint aspernatur velit quod ea nesciunt! cupiditate voluptas laboriosam vitae ex.",
+      },
+      {
+        name: "John Doe",
+        image: "/images/user-1.webp",
+        text: "Lorem ipsum dolor sit! Sint aspernatur velit quod ea nesciunt! cupiditate voluptas laboriosam vitae ex.",
+      },
+      {
+        name: "John Doe",
+        image: "/images/user-1.webp",
+        text: "Lorem ipsum dolor sit! Sint aspernatur velit quod ea nesciunt! cupiditate voluptas laboriosam vitae ex.",
+      },
+    ];
+  }, []);
   return (
-    <div className="testimonials-container">
+    <div className="w-full over-x-hidden testimonials-container px-[8rem] py-[4rem] max-md:px-[2rem] max-md:py-[2rem]">
       <p className="uppercase font-radhiumz text-center text-[15px] max-md:text-[11px] text-color-1/70">
         Testimonial
       </p>
@@ -11,6 +31,25 @@ export default function Testimonials() {
         <br />
         Say About Us.
       </p>
+      <div className="inner flex gap-6 justify-center flex-wrap mt-[40px]">
+        {TESTIMONIALS.map((testimonial, index) => (
+          <div
+            key={index}
+            className="testimonial-card rounded-sm bg-color-main p-4 drop-shadow-lg flex flex-col gap-4 max-w-[280px] flex-[0,0,auto]"
+          >
+            <Image
+              className="rounded-full object-cover size-[50px] mt-[-30px]"
+              src={testimonial.image}
+              alt="testimonial"
+              width={100}
+              height={100}
+            />
+            <p className="text-[12px] max-md:text-[11px] text-color-1/70">
+              {testimonial.text}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
