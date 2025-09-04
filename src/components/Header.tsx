709142ae1677ba7b939b7616acbaf80d6c53c931
victorfaruna <farunavictor043@gmail.com>
2025-09-04 13:57:53 +0100

    change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,139 +5,139 @@ import Link from "next/link";
 import RegionSelector from "./RegionSelector";
 
 export default function Header() {
-  const [headerTheme, setHeaderTheme] = useState<
-    "dark" | "light" | "transparent"
-  >("transparent");
-  const [showHeaderShadow, setShowHeaderShadow] = useState(false);
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+    const [headerTheme, setHeaderTheme] = useState<
+        "dark" | "light" | "transparent"
+    >("transparent");
+    const [showHeaderShadow, setShowHeaderShadow] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setIsDrawerOpen(!isDrawerOpen);
-  };
-
-  useEffect(() => {
-    if (isDrawerOpen) {
-      document.body.style.overflow = "hidden";
-      setHeaderTheme("light");
-    } else {
-      document.body.style.overflow = "auto";
-      setHeaderTheme("transparent");
-    }
-  }, [isDrawerOpen]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 300) {
-        setShowHeaderShadow(true);
-      } else {
-        setShowHeaderShadow(false);
-      }
-      //...
-      if (window.scrollY > 100) {
-        setHeaderTheme("light");
-      } else {
-        setHeaderTheme("transparent");
-      }
+    const handleDrawerToggle = () => {
+        setIsDrawerOpen(!isDrawerOpen);
     };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    useEffect(() => {
+        if (isDrawerOpen) {
+            document.body.style.overflow = "hidden";
+            setHeaderTheme("light");
+        } else {
+            document.body.style.overflow = "auto";
+            setHeaderTheme("transparent");
+        }
+    }, [isDrawerOpen]);
 
-  return (
-    <header
-      className={`${
-        headerTheme === "light"
-          ? "[--header-color-main:rgb(255,255,255);--header-color-1:rgb(40,40,40);--header-bg:rgb(255,255,255,0.6);--header-button-color:rgb(36,103,227)]"
-          : headerTheme === "dark"
-          ? "[--header-color-main]:rgb(0,0,0);[--header-color-1]:rgb(255,255,255);--header-bg:rgb(0,0,0);--header-button-color:rgb(36,103,227)]"
-          : headerTheme === "transparent" &&
-            "[--header-color-main:rgb(255,255,255);--header-color-1:rgb(255,255,255);--header-bg:transparent;--header-button-color:rgba(0,0,0,0.05)]"
-      } ${
-        showHeaderShadow &&
-        "shadow-[0px_0px_200px_rgba(0,0,0,0.2)] backdrop-blur-2xl"
-      }  w-full py-[0.8rem] md:py-[1rem] px-[var(--x-padding)] fixed z-[6] flex items-center justify-between bg-[var(--header-bg)] transition-all duration-700`}
-    >
-      <div className="left w-[250px] flex items-center">
-        <div className="inner flex gap-4 items-center">
-          <Link href={"/"} className="logo">
-            <Logo theme={headerTheme} />
-          </Link>
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 300) {
+                setShowHeaderShadow(true);
+            } else {
+                setShowHeaderShadow(false);
+            }
+            //...
+            if (window.scrollY > 100) {
+                setHeaderTheme("light");
+            } else {
+                setHeaderTheme("transparent");
+            }
+        };
 
-          <div className="cs">
-            <RegionSelector />
-          </div>
-        </div>
-      </div>
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
 
-      <nav
-        className={`${
-          isDrawerOpen
-            ? "max-lg:translate-y-0"
-            : "max-lg:translate-y-[-100%] transition-all duration-700"
-        } text-color-1 lg:text-[var(--header-color-1)] transition-all duration-700 text-[16px] font-medium  absolute lg:relative z-[-1] left-0 top-0`}
-      >
-        <ul className="flex flex-col h-screen lg:h-auto lg:flex-row p-[1rem] pt-[100px] lg:pt-0 w-screen lg:w-auto bg-color-main lg:bg-transparent items-start lg:items-center gap-8">
-          <Link href="/#features">Features</Link>
-          <Link href="/about">About</Link>
-          <Link href="/career">Career</Link>
-          <li className="flex gap-1.5 items-center cursor-pointer">
-            <span>Company</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="2"
-              stroke="currentColor"
-              className="size-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </li>
-        </ul>
-      </nav>
-      <div className="buttons w-[250px] flex justify-end">
-        <button
-          // style={{ textShadow: " 0px 0px 10px rgba(0, 0, 0, 0.4)" }}
-          className="hidden lg:block bg-[var(--header-button-color)] border-[1.5px] border-white/60 backdrop-blur-[5px] transition-all duration-700 hover:cursor-pointer py-[12px] px-[20px] text-[0.9rem] drop-shadow-xl rounded-md self-start text-white font-bold"
+    return (
+        <header
+            className={`${
+                headerTheme === "light"
+                    ? "[--header-color-main:rgb(255,255,255);--header-color-1:rgb(255,255,255);--header-bg:rgb(0,0,0,0.3);--header-button-color:rgb(36,103,227)]"
+                    : headerTheme === "dark"
+                    ? "[--header-color-main]:rgb(0,0,0);[--header-color-1]:rgb(255,255,255);--header-bg:rgb(0,0,0);--header-button-color:rgb(36,103,227)]"
+                    : headerTheme === "transparent" &&
+                      "[--header-color-main:rgb(255,255,255);--header-color-1:rgb(255,255,255);--header-bg:transparent;--header-button-color:rgba(0,0,0,0.05)]"
+            } ${
+                showHeaderShadow &&
+                "shadow-[0px_0px_200px_rgba(0,0,0,0.2)] backdrop-blur-2xl"
+            }  w-full py-[0.8rem] md:py-[1rem] px-[var(--x-padding)] fixed z-[6] flex items-center justify-between bg-[var(--header-bg)] transition-all duration-700`}
         >
-          Get Started- <span className="font-normal">100% free</span>
-        </button>
+            <div className="left w-[250px] flex items-center">
+                <div className="inner flex gap-4 items-center">
+                    <Link href={"/"} className="logo">
+                        <Logo theme={headerTheme} />
+                    </Link>
 
-        <div className="hamburger hidden max-lg:block">
-          <button
-            className="text-[var(--header-color-1)]"
-            onClick={handleDrawerToggle}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-7"
+                    <div className="cs">
+                        <RegionSelector />
+                    </div>
+                </div>
+            </div>
+
+            <nav
+                className={`${
+                    isDrawerOpen
+                        ? "max-lg:translate-y-0"
+                        : "max-lg:translate-y-[-100%] transition-all duration-700"
+                } text-color-1 lg:text-[var(--header-color-1)] transition-all duration-700 text-[16px] font-medium  absolute lg:relative z-[-1] left-0 top-0`}
             >
-              {!isDrawerOpen ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M3.75 9h16.5m-16.5 6.75h16.5"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
-      </div>
-    </header>
-  );
+                <ul className="flex flex-col h-screen lg:h-auto lg:flex-row p-[1rem] pt-[100px] lg:pt-0 w-screen lg:w-auto bg-color-main lg:bg-transparent items-start lg:items-center gap-8">
+                    <Link href="/#features">Features</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/career">Career</Link>
+                    <li className="flex gap-1.5 items-center cursor-pointer">
+                        <span>Company</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="2"
+                            stroke="currentColor"
+                            className="size-4"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                            />
+                        </svg>
+                    </li>
+                </ul>
+            </nav>
+            <div className="buttons w-[250px] flex justify-end">
+                <button
+                    // style={{ textShadow: " 0px 0px 10px rgba(0, 0, 0, 0.4)" }}
+                    className="hidden lg:block bg-[var(--header-button-color)] border-[1.5px] border-white/60 backdrop-blur-[5px] transition-all duration-700 hover:cursor-pointer py-[12px] px-[20px] text-[0.9rem] drop-shadow-xl rounded-md self-start text-white font-bold"
+                >
+                    Get Started- <span className="font-normal">100% free</span>
+                </button>
+
+                <div className="hamburger hidden max-lg:block">
+                    <button
+                        className="text-[var(--header-color-1)]"
+                        onClick={handleDrawerToggle}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-7"
+                        >
+                            {!isDrawerOpen ? (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M3.75 9h16.5m-16.5 6.75h16.5"
+                                />
+                            ) : (
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M6 18 18 6M6 6l12 12"
+                                />
+                            )}
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 }
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,71 +3,75 @@ import Image from "next/image";
 import React from "react";
 
 export default function HeroSection() {
-  return (
-    <div className="outer-container w-full h-[80vh] lg:h-[98vh] [background-image:linear-gradient(151deg,_rgba(0,14,66,0.5)_4%,_rgba(0,14,66,0.1)_80%),url('/images/hero-6.webp')] bg-cover md:[background-size:120%] bg-no-repeat bg-[10%_bottom] lg:bg-[left_top] px-[var(--x-padding)] flex items-center relative overflow-hidden">
-      <div className="hero-text-container w-full lg:w-[55%] flex flex-col gap-8">
-        <h1
-          style={{ textShadow: "0px 0px 10px rgba(0,0,0,0.1)" }}
-          className="text-white text-[2.2rem] md:text-[3.5rem] lg:text-[4.5rem] font-bold leading-none tracking-[-0.02em]"
-        >
-          {/* A financial inclusive world with everyone for everyone. */}
-          Turn your Prepaid SIM into a Virtual Debit Card.
-        </h1>
-        <p className="text-color-main/90 text-[0.9rem] md:text-[1.1rem] font-medium [text-shadow:0px_0px_10px_rgba(0,0,0,0.4)]">
-          We are the Micro-payment app that prioritizes people—individuals,
-          businesses, and stores. Paying is like saying &quot;welcome!&quot;.
-        </p>
-        <div className="flex gap-4 items-center">
-          <button
-            onClick={() =>
-              window.open("https://gimme.fillout.com/waitlist", "_blank")
-            }
-            className="bg-[#2467e3fe] flex items-center justify-center hover:cursor-pointer py-[12px] lg:py-[15px] px-[20px] lg:px-[30px] text-[0.7rem] md:text-[0.9rem] rounded-lg self-start text-white font-bold"
-          >
-            <span>Join Our Waitlist</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
-              />
-            </svg>
-          </button>
+    return (
+        <div className="outer-container w-full h-[70vh] lg:h-[98vh] bg-fixed [background-image:linear-gradient(151deg,_rgba(0,0,5,0.8)_4%,_rgba(0,14,66,0.1)_80%),url('/images/hero-14.webp')] bg-cover md:[background-size:100%] bg-no-repeat bg-[center_bottom] lg:bg-[left_top] px-[var(--x-padding)] flex items-center relative overflow-hidden">
+            <div className="hero-text-container w-full lg:w-[55%] flex flex-col gap-4">
+                <h1
+                    style={{ textShadow: "0px 0px 10px rgba(0,0,0,0.1)" }}
+                    className="font-clash-display pr-2 lg:pr-0 text-white text-[2rem] md:text-[3.5rem] lg:text-[4.5rem] font-semibold leading-none tracking-[-0.02em]"
+                >
+                    A financial inclusive world with everyone for everyone.
+                    {/* Turn your Prepaid SIM into a Virtual Debit Card. */}
+                </h1>
+                <p className="text-color-main/90 text-[0.9rem] md:text-[1.1rem] font-medium [text-shadow:0px_0px_10px_rgba(0,0,0,0.4)]">
+                    We are the Micro-payment app that prioritizes individuals,
+                    businesses, and stores. Paying is like saying
+                    &quot;welcome!&quot;.
+                </p>
+                <div className="flex gap-4 items-center">
+                    <button
+                        onClick={() =>
+                            window.open(
+                                "https://gimme.fillout.com/waitlist",
+                                "_blank"
+                            )
+                        }
+                        className="bg-[#2467e3fe] flex items-center justify-center hover:cursor-pointer py-[12px] lg:py-[15px] px-[20px] lg:px-[30px] text-[0.7rem] md:text-[0.9rem] rounded-lg self-start text-white font-bold"
+                    >
+                        <span>Join Our Waitlist</span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className="play-button-container flex items-center gap-2 group *:transition-all *:duration-700">
+                    <button className="cursor-pointer play-button size-[50px] rounded-full border border-color-main flex items-center justify-center text-color-main group-hover:bg-color-1/80 group-hover:text-color-main/50">
+                        <svg
+                            width="50%"
+                            height="100%"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M4 12V8.44002C4 4.02002 7.13 2.21002 10.96 4.42002L14.05 6.20002L17.14 7.98002C20.97 10.19 20.97 13.81 17.14 16.02L14.05 17.8L10.96 19.58C7.13 21.79 4 19.98 4 15.56V12Z"
+                                fill="CurrentColor"
+                            ></path>
+                        </svg>
+                    </button>
+                    <p className="text-[0.8rem] text-color-main">
+                        Learn more about how <br /> Gimme works
+                    </p>
+                </div>
+            </div>
+            <Image
+                className="w-[2000px] lg:w-[4000px] object-cover absolute z-[4] bottom-0 right-0 max-w-none"
+                src="/abstract/bottom-rule.svg"
+                alt=""
+                width={2000}
+                height={50}
+            />
         </div>
-        <div className="play-button-container flex items-center gap-2 group *:transition-all *:duration-700">
-          <button className="cursor-pointer play-button size-[50px] rounded-full border border-color-main flex items-center justify-center text-color-main group-hover:bg-color-1/80 group-hover:text-color-main/50">
-            <svg
-              width="50%"
-              height="100%"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4 12V8.44002C4 4.02002 7.13 2.21002 10.96 4.42002L14.05 6.20002L17.14 7.98002C20.97 10.19 20.97 13.81 17.14 16.02L14.05 17.8L10.96 19.58C7.13 21.79 4 19.98 4 15.56V12Z"
-                fill="CurrentColor"
-              ></path>
-            </svg>
-          </button>
-          <p className="text-[0.8rem] text-color-main">
-            Learn more about how <br /> Gimme works
-          </p>
-        </div>
-      </div>
-      <Image
-        className="w-[2000px] lg:w-[4000px] object-cover absolute z-[4] bottom-0 right-0 max-w-none"
-        src="/abstract/bottom-rule.svg"
-        alt=""
-        width={2000}
-        height={50}
-      />
-    </div>
-  );
+    );
 }
